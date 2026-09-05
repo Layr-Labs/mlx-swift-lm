728921c08ee9ab32d1a6b656549f5a60fcf58415
Gajesh Naik <26431906+Gajesh2007@users.noreply.github.com>
2026-09-05 12:10:18 -0700

    build: pin merged MLX Swift optimizations

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Layr-Labs/mlx-swift.git",
-                 revision: "789e9e4c3b9ca781be16b6bbc0cc233331b30058"),
+                 revision: "22fa2d5aff9b1ada17c3188c7fe482ca937790ed"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "604.0.0"),
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.23.0"),
         .package(url: "https://github.com/huggingface/swift-huggingface.git", from: "0.9.0"),
